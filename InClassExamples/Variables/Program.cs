05bbd1f0b78cfc11f0c795a71476d6588da6cfdf
Edward Louie <edwardlouie29@ou.edu>
2020-01-22 12:54:12 -0600

    Created variables for firstName, lastName, and favoriteNumber. Also derived crazyNumber from it.

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,29 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstName
+            string firstName;
+
+
+            //Assign a value to firstname (use your name)
+            firstName = "Ed";
+
+
+            //Create an int name favoriteNumber and assign your fav num to it
+            int favoriteNumber = 29;
+            
+            double crazyNumber = 7.6767 * favoriteNumber;
+            
+
+            string lastName = "Louie";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " " + "has favorite number of" + " " + favoriteNumber + " " +
+                                    " \nwhich equates to crazy number of" + " " + crazyNumber);
+
+            // MAKE IT DYNAMIC 
+
+            Console.ReadKey();
         }
     }
 }
